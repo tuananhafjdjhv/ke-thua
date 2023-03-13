a71dc955316e9fb52b8aa7037f7fbfb884ee3fe1
tuananhafjdjhv <119550348+tuananhafjdjhv@users.noreply.github.com>
2023-03-13 23:23:07 +0700

    first commit

---
+++ src/shape/Circle.java
@@ -0,0 +1,35 @@
+package shape;
+
+public class Circle {
+    private double radius = 1.0;
+
+    public double getRadius() {
+        return radius;
+    }
+    public Circle(){
+
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public Circle(double radius,String color,boolean filled){
+        super();
+        this.radius=radius;
+    }
+
+
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+    public double getPerimeter (){
+        return 2*radius*Math.PI;
+    }
+    @Override
+    public String toString() {
+        return "A shape.Circle with radius="
+                + getRadius()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
